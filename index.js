f85926ac3f0758e160ee817c84b02c88cdc7ea38
zahranisa <samy@jehazy.com>
2018-01-07 15:41:38 +0300

    v17

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 'use strict'
 
-exports.register = function (server, options, next) {
-  server.ext('onRequest', function (request, reply) {
+const register = function (server, options) {
+  server.ext('onRequest', function (request, h) {
+    // console.log('request', options, server.info)
     var redirect = options.proxy !== false
       ? request.headers['x-forwarded-proto'] === 'http'
-      : request.connection.info.protocol === 'http'
+      : server.info.protocol === 'http'
     var host = request.headers['x-forwarded-host'] || request.headers.host
 
     if (redirect) {
-      return reply()
-        .redirect('https://' + host + request.url.path)
-        .code(301)
+      const response = h.response().takeover().redirect('https://' + host + request.url.path).code(301)
+      return response
     }
-    reply.continue()
+    return h.continue
   })
-  next()
 }
 
-exports.register.attributes = {
-  pkg: require('./package.json')
+exports.plugin = {
+  pkg: require('./package.json'),
+  register
 }
